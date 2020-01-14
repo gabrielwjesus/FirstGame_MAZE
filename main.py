052534658006ddcd2d4d2650fdf0eca2b69a2190
gabrielwjesus <54075771+gabrielwjesus@users.noreply.github.com>
2020-01-14 14:44:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,51 @@
 from random import randint
 from os import system, name
 
-def Fight(type):
-    life = 100
+class Hero():
+    def __init__(self,life,itens):
+        self.life = life
+        self.itens = itens
 
-    if type == "rat":
-        print("Fighting a Rat")
+    def print_hero(self):
+        print("Hero has %s life" %(self.life))
+        print("Hero is carrying %s", self.itens)
 
-    return life
+    def hero_lose_life(self, lose):
+        self.life = self.life - lose
+        print("Hero lose %s of life and now has %s life" %(lose,self.life))
+
+def Fight(type, hero):
+    life = 10
+
+    while life >> 0 or hero.life == 0 :
+        if type == "rat":
+            print("Fighting a Rat")
+
+            print("It's your turn")
+            dice = Roll_a_Dice()
+            if dice > 5:
+                print("You hit the rat. He lose 2 pts")
+                life = life - 2
+            else:
+                print("Didn't work")
+            print("Keep Fighting. Press ENTER")
+            enter = input()
+
+            print("Now it's Rat's turn")
+            dice = Roll_a_Dice()
+            if dice > 15:
+                print("Hi hit you and you lose 1 pts")
+                hero.hero_lose_life(1)
+            else:
+                print("Didn't work")
+            print("Keep Fighting. Press ENTER")
+            enter = input()
 
 def Roll_a_Dice():
     result = randint(1,20)
     return result
 
-def first_room():
+def first_room(hero):
     print("")
     print("Welcome to your first Room, let's see what you see")
     print("At first you are seeing 2 doors. One Metal and another Wood Door")
@@ -22,20 +54,24 @@ def first_room():
 
     while True:
         print("")
-        print("<<------------------->>")
+        print("<<---------------------------------->>")
+        print("| Personagem com %s pontos de vida   |" %(hero.life))
+        print("| Lista de itens na mochila: %s      |" %(hero.itens))
+        print("<<---------------------------------->>")
         print("Here is your choices")
-        print("1 - Open the Metal door")
-        print("2 - Open the Wooden door")
-        print("3 - Look at the table")
-        print("4 - Look at the library")
+        print("| 1 - Open the Metal door            |")
+        print("| 2 - Open the Wooden door           |")
+        print("| 3 - Look at the table              |")
+        print("| 4 - Look at the library            |")
+        print("<<---------------------------------->>")
         ans = int(input())
 
         if ans == 1:
             print("Ok, entering in the Metal door")
-            going_to_map2()
+            going_to_map2(hero)
         elif ans == 2:
             print("Ok, entering in the Wooden door")
-            going_to_map3()
+            going_to_map3(hero)
         elif ans == 3:
             print("Looking at the table")
             print("Let's try your lucky")
@@ -55,7 +91,7 @@ def first_room():
             else:
                 print(" --- You didn't find anything keep looking --- ")
 
-def going_to_map2():
+def going_to_map2(hero):
     candle = 0
 
     print("")
@@ -66,11 +102,15 @@ def going_to_map2():
 
     while True:
         print("")
-        print("<<------------------->>")
+        print("<<---------------------------------->>")
+        print("| Personagem com %s pontos de vida   |" %(hero.life))
+        print("| Lista de itens na mochila: %s      |" %(hero.itens))
+        print("<<---------------------------------->>")
         print("Here is your choices")
-        print("1 - Open the door")
-        print("2 - Look at the Garbage")
-        print("3 - Look at the Little Candle")
+        print("| 1 - Open the door                  |")
+        print("| 2 - Look at the Garbage            |")
+        print("| 3 - Look at the Little Candle      |")
+        print("<<---------------------------------->>")
         ans = int(input())
 
         if ans == 1:
@@ -78,14 +118,14 @@ def going_to_map2():
                 print("Oh Now, the Door is looked.")
             else:
                 print("The door is opened")
-                going_to_map4()
+                going_to_map4(hero)
         elif ans == 2:
             print("You are looking at the garbage when ... ")
             dice = Roll_a_Dice()
             if dice < 5:
                 print("You find a Big Rat coming at to you")
                 print("Let's Fight")
-                Life = Fight("rat")
+                Fight("rat", hero)
             else:
                 print("You didn't see nothing usefull")
         elif ans == 3:
@@ -97,13 +137,14 @@ def going_to_map2():
             ans2 = int(input())
             if ans2 == 1:
                 print("You burn your hand and loose 2 pt of life")
+                hero.hero_lose_life(2)
             elif ans2 == 2:
                 print("You listened a door unlocking")
                 candle = 1
             elif ans2 == 3:
                 print("Ok, Go ahead")
 
-def going_to_map3():
+def going_to_map3(hero):
     climb = 0
     print("")
     print("Welcome to the next Room, let's see what you see")
@@ -112,11 +153,15 @@ def going_to_map3():
     print("And finally a very messed table")
     while True:
         print("")
-        print("<<------------------->>")
+        print("<<----------------------------------------------------->>")
+        print("| Personagem com %s pontos de vida   |" %(hero.life))
+        print("| Lista de itens na mochila: %s      |" %(hero.itens))
+        print("<<----------------------------------------------------->>")
         print("Here is your choices")
-        print("1 - Open the door")
-        print("2 - Look better for the top to try to find something")
-        print("3 - Look at the table")
+        print("| 1 - Open the door                                     |")
+        print("| 2 - Look better for the top to try to find something  |")
+        print("| 3 - Look at the table                                 |")
+        print("<<----------------------------------------------------->>")
         ans = int(input())
 
         if ans == 1:
@@ -156,20 +201,21 @@ def going_to_map3():
             else:
                 print("Didn't find anything")
 
-def going_to_map4():
+def going_to_map4(hero):
     pass
 
-def going_to_map5():
+def going_to_map5(hero):
     pass
 
 def start():
+    hero = Hero(30,[])
     print("\n")
     print("You are in your first room.")
     print("Pay atenttion for this rule")
     print("YOU NEVER COME BACK")
     print("Find the EXIT")
     print("Or DYE trying")
-    first_room()
+    first_room(hero)
 
 def init_game():
     print ("Welcome to RPGMaze")
